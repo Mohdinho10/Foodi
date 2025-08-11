bd0852feab42930744b0c1a2e45f6b91f7f8058a
mohdinho10 <muhammadqulatain123@gmail.com>
2025-08-11 23:18:55 +0300

    Trying removing *

--- backend/middleware/verifyAdmin.js
+++ backend/middleware/verifyAdmin.js
@@ -1,17 +1,16 @@
-import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 export const verifyAdmin = async (req, res, next) => {
-    const email = req.decoded.email;
-    const query ={email: email};
+  const email = req.decoded.email;
+  const query = { email: email };
 
-    const user = await User.findOne(query);
-    console.log(user)
-    const isAdmin = user?.role == 'admin';
+  const user = await User.findOne(query);
+  console.log(user);
+  const isAdmin = user?.role == "admin";
 
-    if(!isAdmin){
-        return res.status(403).send({message: "forbidden access!"})
-    }
+  if (!isAdmin) {
+    return res.status(403).send({ message: "forbidden access!" });
+  }
 
-    next();
+  next();
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,16 +7,16 @@ import {
   getAdmin,
   getAllUsers,
   makeAdmin,
-  logout
+  logout,
 } from "../controllers/userController.js";
 
 const router = Router();
 
-router.get("/", verifyToken, verifyAdmin,  getAllUsers);
+router.get("/", verifyToken, verifyAdmin, getAllUsers);
 router.post("/", createUser);
-router.post("/logout", logout)
-router.delete("/:id",verifyToken, verifyAdmin,   deleteUser);
-router.get("/admin/:email",verifyToken,  getAdmin);
-router.patch("/admin/:id",verifyToken, verifyAdmin,   makeAdmin);
+router.post("/logout", logout);
+router.delete("/:id", verifyToken, verifyAdmin, deleteUser);
+router.get("/admin/:email", verifyToken, getAdmin);
+router.patch("/admin/:id", verifyToken, verifyAdmin, makeAdmin);
 
 export default router;
