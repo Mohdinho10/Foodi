c2011fed2926d873f5feebc24aee0d84f84fd46d
mohdinho10 <muhammadqulatain123@gmail.com>
2025-08-11 23:29:55 +0300

    Trying new way

--- backend/app.js
+++ backend/app.js
@@ -91,9 +91,10 @@ app.post("/create-payment-intent", async (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-  // app.get("/*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  // });
+  // Catch-all for frontend routes except API
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 // Error handlers
