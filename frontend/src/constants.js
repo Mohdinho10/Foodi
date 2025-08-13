294fa7db4d5c0355d791a4e51823cbb9280a2a94
mohdinho10 <muhammadqulatain123@gmail.com>
2025-08-13 07:47:00 +0300

    Adding api URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,8 @@
 export const BASE_URL =
   // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://foodi-zkdc.onrender.com";
 
 export const MENU_URL = "/api/menu";
 export const CART_URL = "/api/cart";
