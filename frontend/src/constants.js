9de3a80d509ebf4af9f10bd8d749e6c38f1417d1
mohdinho10 <muhammadqulatain123@gmail.com>
2025-08-13 07:52:14 +0300

    Forcing

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,9 @@
-export const BASE_URL =
-  // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://foodi-zkdc.onrender.com";
+// export const BASE_URL =
+//   // eslint-disable-next-line no-undef
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:3000"
+//     : "https://foodi-zkdc.onrender.com";
+export const BASE_URL = "https://foodi-zkdc.onrender.com";
 
 export const MENU_URL = "/api/menu";
 export const CART_URL = "/api/cart";
